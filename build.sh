646808fbba9790944a30edde638b9036a184d3cc
Aleen <aleen42@vip.qq.com>
2021-07-09 22:57:16 +0800

    chore: release _asset in the main branch

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Copy fonts
+cp -R node_modules/font-awesome/fonts/ _assets/website/fonts/fontawesome/
+
+# Copy icons
+cp node_modules/gitbook-logos/output/apple-touch-icon-152.png _assets/website/images/apple-touch-icon-precomposed-152.png
